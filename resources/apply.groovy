e0d7eca8b9678e3b44e90ba80ed2f3b62339f20b
Min Zhou <coderplay@gmail.com>
2015-06-12 17:42:56 -0700

    This is just a test commitment for `Emacs Magit`
    ```java
    
      private static boolean contentsEquals(InputStream in1, InputStream in2)
          throws IOException {
        if (!(in1 instanceof BufferedInputStream)) {
          in1 = new BufferedInputStream(in1);
        }
        if (!(in2 instanceof BufferedInputStream)) {
          in2 = new BufferedInputStream(in2);
        }
    
        int ch = in1.read();
        while (ch != -1) {
          int ch2 = in2.read();
          if (ch != ch2) {
            return false;
          }
          ch = in1.read();
        }
        int ch2 = in2.read();
        return ch2 == -1;
      }
    
    ```

---
+++ resources/apply.groovy
@@ -0,0 +1,17 @@
+buildscript {
+  repositories {
+    maven {
+      url 'http://dl.bintray.com/cjstehno/public'
+    }
+  }
+  dependencies {
+    classpath 'gradle-templates:gradle-templates:1.5'
+  }
+}
+// Check to make sure templates.TemplatesPlugin isn't already added.
+if (!project.plugins.findPlugin(templates.TemplatesPlugin)) {
+  project.apply(plugin: templates.TemplatesPlugin)
+}
+
+
+apply plugin:'templates'
